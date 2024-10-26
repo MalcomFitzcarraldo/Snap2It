7b7ec87eb5c04853b1fd9b09f5c16361dd302112
Malcom Fitzcarraldo <drakep@live.com>
2024-10-26 16:00:54 -0500

    preq.bat

--- preq.bat
+++ preq.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Install required Python packages
-pip install pillow colorama pyperclip pytesseract keyboard subprocess
+pip install pillow colorama pyperclip pytesseract keyboard subprocess ollama
 echo All packages have been installed.
 echo SAFE TO CLOSE
 pause
