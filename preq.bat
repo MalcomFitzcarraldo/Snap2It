5816da3d96e20ae285b55c1b932b86c653ed3bb8
Malcom Fitzcarraldo <drakep@live.com>
2024-10-27 08:47:45 -0500

    Delete preq.bat

--- preq.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Install required Python packages
-pip install pillow colorama pyperclip pytesseract keyboard subprocess ollama
-echo All packages have been installed.
-echo SAFE TO CLOSE
-pause
