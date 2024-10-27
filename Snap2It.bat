eeda6bbc55d0bf89e9ecd557bf54f5773c920177
Malcom Fitzcarraldo <drakep@live.com>
2024-10-27 09:20:10 -0500

    Update Snap2It.bat

--- Snap2It.bat
+++ Snap2It.bat
@@ -1,11 +1,10 @@
 @echo off
 if exist "C:\snap2it" (
     Success.
-START watcher.py
+START C:\snap2it\watcher.py
 ) else (
 color C
     echo ERROR 404 - Not Installed.
-    echo Run setup.bat
     echo The Snap2It files must be in "C:\snap2it\".
 pause
     
